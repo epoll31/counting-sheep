75c96e40e609803576d44ae06d605441e6498314
ethan <epollack31@gmail.com>
2024-03-19 20:29:39 -0700

    updated sheep animations: running and jumping / better input

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -3,7 +3,8 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
-    sheep: any;
+    sheep: Phaser.Physics.Arcade.Sprite;
+    inAir: boolean = true;
 
     constructor() {
         super('Game');
@@ -18,16 +19,46 @@ export class Game extends Scene {
         this.camera.setBackgroundColor(0x9ed3fe);
         // this.add.image(10, 10, 'sheep').setOrigin(0).setDepth(100).setScale(0.2);
 
-        const tags = this.anims.createFromAseprite('sheep');
-        console.log(tags);
-        const sprite = this.add.sprite(512, 383, 'sheep').setOrigin(0.5).setScale(5);
-        sprite.play({
-            key: 'running',
-            // frameRate: 60,
-            repeat: -1
+        this.anims.createFromAseprite('sheep');
+        this.sheep = this.physics.add.sprite(512, 383, 'sheep').setOrigin(0.5).setScale(5).setGravityY(400).setCollideWorldBounds(true, undefined, 0.3, true);
+        this.sheep.play({
+            key: 'jumping',
+            repeat: -1,
+        });
+
+        const jump = (_: any) => {
+            if (!this.inAir) {
+                this.sheep.setVelocityY(-400);
+                this.inAir = true;
+                this.sheep.anims.play({ key: 'jumping', repeat: -1 });
+            }
+        }
+
+        const spaceBar = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        const upKey = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        const wKey = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
+        spaceBar?.on('down', jump);
+        upKey?.on('down', jump);
+        wKey?.on('down', jump);
+        this.input.on('pointerdown', jump);
+
+        this.physics.world.on("worldbounds", (body: Phaser.Physics.Arcade.Body, up: boolean, down: boolean, left: boolean, right: boolean) => {
+            const { gameObject } = body;
+
+            if (gameObject === this.sheep) {
+                if (down && this.inAir && Math.abs(this.sheep.body?.velocity.y ?? 0) < 200) { //130 ~ second bounce, 200 ~ first bounce
+                    this.sheep.anims.play({ key: 'running', repeat: -1 });
+                    this.inAir = false;
+                }
+            }
         });
 
         EventBus.emit('current-scene-ready', this);
+    }
+
+    update() {
+        // console.log(this.sheep.body?.velocity.y);
     }
 
 
